f6c7e2ae38897d092338c44a4a692dd66186ecdb
SillyFreak <sillyfreak.ck@gmail.com>
2018-10-08 02:31:15 +0200

    add pipe & Component

--- task_utils/__init__.py
+++ task_utils/__init__.py
@@ -1,15 +1,181 @@
-def good_foo():
-    """\
-    >>> good_foo()
-    0
-    """
-    return 0
+import asyncio
 
-def bad_foo():
+
+class EOFError(Exception): pass
+
+
+def pipe(maxsize=0):
     """\
-    >>> bad_foo()
+    A bidirectional pipe of Python objects.
+
+    >>> a, b = pipe()
+    >>> a.send_nowait('foo')
+    >>> asyncio.run(b.recv())
+    'foo'
+    >>> asyncio.run(b.send(eof=True))
+    >>> asyncio.run(a.recv())
     Traceback (most recent call last):
       ...
-    ValueError
+    task_utils.EOFError
     """
-    raise ValueError
+
+    _none = object()
+    EOF = object()
+
+    class Pipe:
+        def __init__(self, send, recv):
+            self._send = send
+            self._recv = recv
+
+        def send_nowait(self, value=EOF, *, eof=False):
+            if value is EOF and not eof:
+                raise ValueError("Missing value or EOF")
+            if value is not EOF and eof:
+                raise ValueError("value and EOF are mutually exclusive")
+
+            self._send.put_nowait(value)
+
+        async def send(self, value=EOF, *, eof=False):
+            if value is EOF and not eof:
+                raise ValueError("Missing value or EOF")
+            if value is not EOF and eof:
+                raise ValueError("value and EOF are mutually exclusive")
+
+            await self._send.put(value)
+
+        async def recv(self):
+            result = await self._recv.get()
+            if result is EOF:
+                raise EOFError
+            return result
+
+        async def request_sendnowait(self, value):
+            self.send_nowait(value)
+            return await self.recv()
+
+        async def request(self, value):
+            await self.send(value)
+            return await self.recv()
+
+
+    a, b = asyncio.Queue(maxsize), asyncio.Queue(maxsize)
+    return Pipe(a, b), Pipe(b, a)
+
+
+class LifecycleError(Exception): pass
+class Success(Exception): pass
+class Failure(Exception): pass
+class EventException(Exception): pass
+
+EVENT_START = 'EVENT_START'
+COMMAND_STOP = 'COMMAND_STOP'
+
+
+class Component:
+    """\
+    A task extended with pipes for communication and a lifecycle.
+    Pass a coroutine function and arguments to Component;
+    upon `start()` the coroutine function will be started as a task with two additional pipe arguments:
+    `commands` is for caller-initiated communication; `events` for task-initiated communication.
+
+    There are two lifecycle requirements to the passed coroutine function:
+    - when the task starts, `EVENT_START` needs to be sent; and
+    - when `COMMAND_STOP` is received, the task should prepare for teardown. If the task refuses to stop,
+      it needs to send an event of its choice, otherwise it must terminate.
+    """
+
+    def __init__(self, coro_func, *args, **kwargs):
+        self._commands, commands = pipe()
+        self._events, events = pipe()
+        self._coro_func = lambda: self._coro_wrapper(coro_func, *args, commands=commands, events=events, **kwargs)
+        self.task = None
+
+    async def _coro_wrapper(self, coro_func, *args, commands, events, **kwargs):
+        try:
+            result = await coro_func(*args, commands=commands, events=events, **kwargs)
+        except Exception as err:
+            raise Failure(err) from None
+        else:
+            raise Success(result)
+        finally:
+            events.send_nowait(eof=True)
+
+    async def start(self):
+        """\
+        Start the component; waits for `EVENT_START` and raises `LifecycleError` if the task completes without it,
+        or sends a different event first.
+        """
+        self.task = asyncio.create_task(self._coro_func())
+        try:
+            start_event = await self.recv_event()
+        except Success as succ:
+            raise LifecycleError(f"component returned before start finished") from succ
+        except Failure as fail:
+            # here we don't expect a wrapped result, so we unwrap the failure
+            cause, = fail.args
+            raise cause from None
+        else:
+            if start_event != EVENT_START:
+                raise LifecycleError(f"Component must emit EVENT_START, was {start_event}")
+
+    async def stop(self):
+        """\
+        Stop the component; sends `COMMAND_STOP` and returns `result()`.
+        """
+        self.send(COMMAND_STOP)
+        return await self.result()
+
+    async def result(self):
+        """\
+        Wait for the task's termination; either the result is returned or a raise exception is reraised.
+        If an event is sent before the task terminates, a recoverable `EventException` is raised
+        with the event as argument.
+        """
+        try:
+            event = await self.recv_event()
+        except Success as succ:
+            # success was thrown; return the result
+            result, = succ.args
+            return result
+        except Failure as fail:
+            # here we don't expect a wrapped result, so we unwrap the failure
+            cause, = fail.args
+            raise cause
+        else:
+            # there was a regular event; shouldn't happen/is exceptional
+            raise EventException(event)
+
+    def send(self, value):
+        """\
+        Sends a command to the task.
+        """
+        self._commands.send_nowait(value)
+
+    async def recv(self):
+        """\
+        Receives a command reply from the task.
+        """
+        return await self._commands.recv()
+
+    async def request(self, value):
+        """\
+        Sends a command to and receives the reply from the task.
+        """
+        return await self._commands.request(value)
+
+    async def recv_event(self):
+        """\
+        Receives an event from the task.
+        If the task terminates before another event, an exception is raised.
+        A normal return is wrapped in a `Success` exception, other exceptions are simply raised.
+        """
+        try:
+            return await self._events.recv()
+        except EOFError:
+            # component has terminated, raise the cause
+            # either Success or a regular error
+            self.task.result()
+            assert False, f"EOF sent to event queue manually"
+
+    def send_event_reply(self, value):
+        self._events.send_nowait(value)
--- tests/test_task_utils.py
+++ tests/test_task_utils.py
@@ -3,8 +3,164 @@ import pytest
 import task_utils
 
 
-def test_task_utils():
-    assert task_utils.good_foo() == 0
+@pytest.mark.asyncio
+async def test_pipe():
+    a, b = task_utils.pipe()
 
-    with pytest.raises(ValueError):
-        task_utils.bad_foo()
+    a.send_nowait(1)
+    assert await b.recv() == 1
+    await b.send(2)
+    assert await a.recv() == 2
+
+
+@pytest.mark.asyncio
+async def test_component_start_returns():
+    async def component(x, *, commands, events):
+        return x
+
+    comp = task_utils.Component(component, 1)
+    with pytest.raises(task_utils.LifecycleError) as exc_info:
+        await comp.start()
+
+    result, = exc_info.value.__cause__.args
+    assert result == 1
+
+
+@pytest.mark.asyncio
+async def test_component_start_raises():
+    class Fail(Exception): pass
+
+    async def component(x, *, commands, events):
+        raise Fail
+
+    comp = task_utils.Component(component, 1)
+    with pytest.raises(Fail) as exc_info:
+        await comp.start()
+
+
+@pytest.mark.asyncio
+async def test_component_result_success():
+    async def component(x, *, commands, events):
+        events.send_nowait(task_utils.EVENT_START)
+        return x
+
+    comp = task_utils.Component(component, 1)
+    await comp.start()
+    assert await comp.result() == 1
+
+
+@pytest.mark.asyncio
+async def test_component_result_exception():
+    class Fail(Exception): pass
+
+    async def component(x, *, commands, events):
+        events.send_nowait(task_utils.EVENT_START)
+        raise Fail
+
+    comp = task_utils.Component(component, 1)
+    await comp.start()
+    with pytest.raises(Fail):
+        await comp.result()
+
+
+@pytest.mark.asyncio
+async def test_component_result_event():
+    async def component(x, *, commands, events):
+        events.send_nowait(task_utils.EVENT_START)
+        events.send_nowait('EVENT')
+        return x
+
+    comp = task_utils.Component(component, 1)
+    await comp.start()
+    with pytest.raises(task_utils.EventException):
+        await comp.result()
+    assert await comp.result() == 1
+
+
+@pytest.mark.asyncio
+async def test_component_stop_success():
+    async def component(x, *, commands, events):
+        events.send_nowait(task_utils.EVENT_START)
+        command = await commands.recv()
+        assert command == task_utils.COMMAND_STOP
+        return x
+
+    comp = task_utils.Component(component, 1)
+    await comp.start()
+    assert await comp.stop() == 1
+
+
+@pytest.mark.asyncio
+async def test_component_stop_exception():
+    class Fail(Exception): pass
+
+    async def component(x, *, commands, events):
+        events.send_nowait(task_utils.EVENT_START)
+        command = await commands.recv()
+        assert command == task_utils.COMMAND_STOP
+        raise Fail
+
+    comp = task_utils.Component(component, 1)
+    await comp.start()
+    with pytest.raises(Fail):
+        await comp.stop()
+
+
+@pytest.mark.asyncio
+async def test_component_stop_event():
+    async def component(x, *, commands, events):
+        events.send_nowait(task_utils.EVENT_START)
+        events.send_nowait('EVENT')
+        return x
+
+    comp = task_utils.Component(component, 1)
+    await comp.start()
+    with pytest.raises(task_utils.EventException):
+        await comp.stop()
+    assert await comp.stop() == 1
+
+
+@pytest.mark.asyncio
+async def test_component_recv_event():
+    async def component(x, *, commands, events):
+        events.send_nowait(task_utils.EVENT_START)
+        events.send_nowait('EVENT')
+        return x
+
+    comp = task_utils.Component(component, 1)
+    await comp.start()
+    assert await comp.recv_event() == 'EVENT'
+    assert await comp.result() == 1
+
+
+@pytest.mark.asyncio
+async def test_component_recv_event_return():
+    async def component(x, *, commands, events):
+        events.send_nowait(task_utils.EVENT_START)
+        return x
+
+    comp = task_utils.Component(component, 1)
+    await comp.start()
+    with pytest.raises(task_utils.Success) as exc_info:
+        await comp.recv_event()
+
+    result, = exc_info.value.args
+    assert result == 1
+
+
+@pytest.mark.asyncio
+async def test_component_recv_event_raise():
+    class Fail(Exception): pass
+
+    async def component(x, *, commands, events):
+        events.send_nowait(task_utils.EVENT_START)
+        raise Fail
+
+    comp = task_utils.Component(component, 1)
+    await comp.start()
+    with pytest.raises(task_utils.Failure) as exc_info:
+        await comp.recv_event()
+
+    fail, = exc_info.value.args
+    with pytest.raises(Fail):
+        raise fail
