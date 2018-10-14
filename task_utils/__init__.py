d185e06c7f3bd0b2f048829848301bd2d1a906e1
SillyFreak <sillyfreak.ck@gmail.com>
2018-10-14 11:30:17 +0200

    extract a visible base class for the Pipe implementation

--- task_utils/__init__.py
+++ task_utils/__init__.py
@@ -1,8 +1,40 @@
+from abc import abstractmethod
 import asyncio
 import asyncio.locks
 
 
 class EOFError(Exception): pass
+
+
+class Pipe:
+    _none = object()
+
+    @staticmethod
+    def check_send_args(value, *, eof):
+        if value is Pipe._none and not eof:
+            raise ValueError("Missing value or EOF")
+        if value is not Pipe._none and eof:
+            raise ValueError("value and EOF are mutually exclusive")
+
+    @abstractmethod
+    def send_nowait(self, value=_none, *, eof=False):
+        raise NotImplemented
+
+    @abstractmethod
+    async def send(self, value=_none, *, eof=False):
+        raise NotImplemented
+
+    @abstractmethod
+    async def recv(self):
+        raise NotImplemented
+
+    async def request_sendnowait(self, value):
+        self.send_nowait(value)
+        return await self.recv()
+
+    async def request(self, value):
+        await self.send(value)
+        return await self.recv()
 
 
 def pipe(maxsize=0):
@@ -25,22 +57,17 @@ def pipe(maxsize=0):
     task_utils.EOFError
     """
 
-    _none = object()
-
     class QueueStream:
         def __init__(self, maxsize=0):
             self._queue = asyncio.Queue(maxsize)
             self._eof = asyncio.locks.Event()
 
-        def _check_send(self, value=_none, *, eof=False):
-            if value is _none and not eof:
-                raise ValueError("Missing value or EOF")
-            if value is not _none and eof:
-                raise ValueError("value and EOF are mutually exclusive")
+        def _check_send(self, value, *, eof):
             if self._eof.is_set():
                 raise EOFError("Cannot send after EOF")
+            Pipe.check_send_args(value, eof=eof)
 
-        def send_nowait(self, value=_none, *, eof=False):
+        def send_nowait(self, value, *, eof):
             self._check_send(value, eof=eof)
 
             if eof:
@@ -48,7 +75,7 @@ def pipe(maxsize=0):
             else:
                 self._queue.put_nowait(value)
 
-        async def send(self, value=_none, *, eof=False):
+        async def send(self, value, *, eof):
             self._check_send(value, eof=eof)
 
             if eof:
@@ -71,30 +98,23 @@ def pipe(maxsize=0):
             else:
                 raise EOFError
 
-    class Pipe:
+    class _Pipe(Pipe):
         def __init__(self, send, recv):
+            super().__init__()
             self._send = send
             self._recv = recv
 
-        def send_nowait(self, value=_none, *, eof=False):
+        def send_nowait(self, value=Pipe._none, *, eof=False):
             self._send.send_nowait(value, eof=eof)
 
-        async def send(self, value=_none, *, eof=False):
+        async def send(self, value=Pipe._none, *, eof=False):
             await self._send.send(value, eof=eof)
 
         async def recv(self):
             return await self._recv.recv()
 
-        async def request_sendnowait(self, value):
-            self.send_nowait(value)
-            return await self.recv()
-
-        async def request(self, value):
-            await self.send(value)
-            return await self.recv()
-
     a, b = QueueStream(maxsize), QueueStream(maxsize)
-    return Pipe(a, b), Pipe(b, a)
+    return _Pipe(a, b), _Pipe(b, a)
 
 
 class Component:
