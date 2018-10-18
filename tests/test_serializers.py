abd65d847d94b2443595a43d26d69e43c2cb1301
SillyFreak <sillyfreak.ck@gmail.com>
2018-10-18 12:47:30 +0200

    add test_serializers

---
+++ tests/test_serializers.py
@@ -0,0 +1,31 @@
+import pytest
+from dataclasses import dataclass
+
+from task_utils.serializers import Pickle, Msgpack
+
+
+@dataclass
+class Dataclass: pass
+
+
+def test_pickle():
+    serializer = Pickle()
+
+    for value in ['foo', 1, [0.1], {'baz': True}, (), {'bar'}, Dataclass()]:
+        assert serializer.deserialize(serializer.serialize(value)) == value
+
+
+def test_msgpack():
+    serializer = Msgpack()
+
+    for value in ['foo', 1, [0.1], {'baz': True}]:
+        assert serializer.deserialize(serializer.serialize(value)) == value
+
+    # tuples are serialized as lists
+    for value in [()]:
+        assert serializer.deserialize(serializer.serialize(value)) != value
+
+    # sets & custom classes are not supported
+    for value in [{'bar'}, Dataclass()]:
+        with pytest.raises(TypeError):
+            serializer.serialize(value)
