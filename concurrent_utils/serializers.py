fce6d88aed0655af65e8d93b2f2466a3498f8a78
SillyFreak <sillyfreak.ck@gmail.com>
2018-10-28 15:37:13 +0100

    clean up setup.cfg; no manual nocover for unimplemented methods any more

--- concurrent_utils/serializers.py
+++ concurrent_utils/serializers.py
@@ -12,11 +12,11 @@ __all__ = [
 class Serializer:
     @abstractmethod
     def serialize(self, value: Any) -> bytes:
-        raise NotImplemented  # pragma: nocover
+        raise NotImplemented
 
     @abstractmethod
     def deserialize(self, msg: bytes) -> Any:
-        raise NotImplemented  # pragma: nocover
+        raise NotImplemented
 
 
 try:  # pragma: nocover
