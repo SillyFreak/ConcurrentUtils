01377e09bbfc7ebc78031bc8e4c4939ee2f85ff4
SillyFreak <sillyfreak.ck@gmail.com>
2018-10-29 10:51:17 +0100

    prepare release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='concurrent-utils',
-    version='0.0.0',
+    version='0.1.0',
     description='concurrent-utils',
     long_description=long_description,
     url="https://github.com/SillyFreak/ConcurrentUtils",
